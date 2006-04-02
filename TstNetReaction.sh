ad9f4dde1497950988aabb8897725567f0a392dd
jeffries <jeffries@f2e443aa-f90a-0410-907f-bad4a36f275b>
2006-04-02 21:50:04 +0000

    Expanded test to both Clinton and Source Regions
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/net_balance/trunk@15 f2e443aa-f90a-0410-907f-bad4a36f275b

--- TstNetReaction.sh
+++ TstNetReaction.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
 python net_balance.py  ../inout/camx403.20000830.base5b.psito2n2.1km.clinton.1km.hourly.cycle.8.18.ext \
-  ../inout/Net_balance_output.txt
+  ../inout/Net_balance_clinton.txt
+
+python net_balance.py  ../inout/camx403.20000830.base5b.psito2n2.1km.source.1km.hourly.cycle.8.18.ext \
+  ../inout/Net_balance_source.txt
