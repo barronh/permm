3545842a62eeb07cd46ce0476c8304a198e2e777
jeffries <jeffries@f2e443aa-f90a-0410-907f-bad4a36f275b>
2006-04-11 18:52:21 +0000

    hej full version of tst shell
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/net_balance/trunk@44 f2e443aa-f90a-0410-907f-bad4a36f275b

--- TstNetReaction.sh
+++ TstNetReaction.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-#python net_balance.py  ../inout/camx403.20000830.base5b.psito2n2.1km.clinton.1km.hourly.cycle.8.18.ext \
-#  ../inout/Clinton_1hour_0830_818_403.txt
+python net_balance.py  ../inout/camx403.20000830.base5b.psito2n2.1km.clinton.1km.hourly.cycle.8.18.ext \
+  ../inout/Clinton_1hour_0830_818_403.txt
 
-#python net_balance.py  ../inout/camx420_pa.20000830.base1b.psito2n2.4km.clinton.hourly.cycle.8.18.ext \
-#  ../inout/Clinton_8hour_0830_818_420.txt
+python net_balance.py  ../inout/camx420_pa.20000830.base1b.psito2n2.4km.clinton.hourly.cycle.8.18.ext \
+  ../inout/Clinton_8hour_0830_818_420.txt
 
 python net_balance.py  ../inout/camx420_pa.20000830.base1b.4time_co.4km.clinton.hourly.cycle.8.18.ext \
   ../inout/Clinton_4timeCO_0830_818_420.txt
@@ -12,10 +12,10 @@ python net_balance.py  ../inout/camx420_pa.20000830.base1b.quart_co.4km.clinton.
   ../inout/Clinton_quartCO_0830_818_420.txt
 
 python net_balance.py  ../inout/camx420_pa.20000825.base1b.psito2n2.4km.bayland.hourly.cycle.8.18.ext \
-  ../inout/Bayland_8hour_0830_818_420.txt
+  ../inout/Bayland_8hour_0825_818_420.txt
 
 python net_balance.py  ../inout/camx420_pa.20000825.base1b.4time_co.4km.bayland.hourly.cycle.8.18.ext \
-  ../inout/Bayland_4timeCO_0830_818_420.txt
+  ../inout/Bayland_4timeCO_0825_818_420.txt
 
 python net_balance.py  ../inout/camx420_pa.20000825.base1b.quart_co.4km.bayland.hourly.cycle.8.18.ext \
   ../inout/Bayland_quartCO_0825_818_420.txt
