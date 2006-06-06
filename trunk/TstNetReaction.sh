be2fa3bdd07f75982ad6aaf94263eab01e8d3a54
svnjeffries <svnjeffries@f2e443aa-f90a-0410-907f-bad4a36f275b>
2006-06-06 01:40:20 +0000

    Release for orginal version of the mrgaloft file
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/trunk@107 f2e443aa-f90a-0410-907f-bad4a36f275b

---
+++ trunk/TstNetReaction.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+## $LastChangedDate: 2006-04-24 11:56:17 -0400 (Mon, 24 Apr 2006) $
+## $LastChangedRevision: 115 $
+## $LastChangedBy: jeffries $
+
+
+python net_balance.py  ../inout/X420U.000830.b1b.TQ.std.clinton.hourly.cycle.8.18.ext \
+  ../inout/Clinton_8h_000830_0818h.txt
+
+python net_balance.py  ../inout/X420U.000825.b1b.TQ.std.bayland.hourly.cycle.8.18.ext \
+  ../inout/Bayland_8h_000825_0818h.txt
+
+python net_balance.py  ../inout/X420U.000825.b1b.TQ.std.HALC.hourly.cycle.8.18.ext \
+  ../inout/HALC_8h_000825_0818h.txt
+
+python net_balance.py  ../inout/X420U.000830.b1b.TQ.std.HALC.hourly.cycle.8.18.ext \
+  ../inout/HALC_8h_000830_0818h.txt
+  
+python net_balance.py  ../inout/X420U.000830.b1b.TQ.std.DRPK.hourly.cycle.8.18.ext \
+  ../inout/DRPK_8h_000830_0818h.txt  
---
+++ trunk/TstNetReactionOne.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+python net_balance.py  ../inout/X420U.000825.b1b.TQ.std.bayland.hourly.cycle.8.18.ext \
+  ../inout/Bayland_8h_000825_0818h
+
+
+
+
