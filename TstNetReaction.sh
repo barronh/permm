24e929ef0528457754d7a4d2a8fdb277ad976699
svnjeffries <svnjeffries@f2e443aa-f90a-0410-907f-bad4a36f275b>
2006-07-13 18:44:22 +0000

    Added VOC output.  Added split transport to .phy file. Several "num_" variables made consistent.
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/trunk@112 f2e443aa-f90a-0410-907f-bad4a36f275b

--- TstNetReaction.sh
+++ TstNetReaction.sh
@@ -5,17 +5,17 @@
 ## $LastChangedBy: jeffries $
 
 
-python net_balance.py  ../inout/X420U.000830.b1b.TQ.std.clinton.hourly.cycle.8.18.ext \
+python net_balance_CB4.py  ../inout/X420U.000830.b1b.TQ.std.clinton.hourly.cycle.8.18.ext \
   ../inout/Clinton_8h_000830_0818h.txt
 
-python net_balance.py  ../inout/X420U.000825.b1b.TQ.std.bayland.hourly.cycle.8.18.ext \
+python net_balance_CB4.py  ../inout/X420U.000825.b1b.TQ.std.bayland.hourly.cycle.8.18.ext \
   ../inout/Bayland_8h_000825_0818h.txt
 
-python net_balance.py  ../inout/X420U.000825.b1b.TQ.std.HALC.hourly.cycle.8.18.ext \
+python net_balance_CB4.py  ../inout/X420U.000825.b1b.TQ.std.HALC.hourly.cycle.8.18.ext \
   ../inout/HALC_8h_000825_0818h.txt
 
-python net_balance.py  ../inout/X420U.000830.b1b.TQ.std.HALC.hourly.cycle.8.18.ext \
+python net_balance_CB4.py  ../inout/X420U.000830.b1b.TQ.std.HALC.hourly.cycle.8.18.ext \
   ../inout/HALC_8h_000830_0818h.txt
   
-python net_balance.py  ../inout/X420U.000830.b1b.TQ.std.DRPK.hourly.cycle.8.18.ext \
+python net_balance_CB4.py  ../inout/X420U.000830.b1b.TQ.std.DRPK.hourly.cycle.8.18.ext \
   ../inout/DRPK_8h_000830_0818h.txt  
