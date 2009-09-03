0160a7f4ece398a3c6e864aca5530798d5d125e9
barronh <barronh@f2e443aa-f90a-0410-907f-bad4a36f275b>
2009-09-03 18:51:01 +0000

    netCDF4 is now preferred
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/trunk@232 f2e443aa-f90a-0410-907f-bad4a36f275b

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except:
 import os
 import sys
 from warnings import warn
-netcdf_pkgs = [('pynetcdf', 'NetCDFFile'), ('netCDF3', 'Dataset'), \
-               ('netCDF4', 'Dataset'), ('Scientific.IO.NetCDF', 'NetCDFFile'), \
+netcdf_pkgs = [('netCDF4', 'Dataset'), \
+               ('netCDF3', 'Dataset'), \
                ('pupynere', 'NetCDFFile')]
 for pkg, reader in netcdf_pkgs:
     try:
