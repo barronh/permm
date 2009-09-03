cbbd27f93964680b2d461e4e921d243165360b34
barronh <barronh@f2e443aa-f90a-0410-907f-bad4a36f275b>
2009-09-03 18:51:00 +0000

    netCDF4 is now preferred
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/trunk@231 f2e443aa-f90a-0410-907f-bad4a36f275b

--- src/perm/netcdf.py
+++ src/perm/netcdf.py
@@ -1,1 +1,16 @@
-from pynetcdf import NetCDFFile
+
+__all__ = ['NetCDFFile']
+__doc__ = """
+.. _netcdf
+:mod:`netcdf` -- netcdf import point
+====================================
+
+.. module:: netcdf
+   :platform: Unix, Windows
+   :synopsis: Povides a single import point for a package.  If
+              a user has one of many netcdf interfaces, this module
+              selects it and provides it.
+.. moduleauthor:: Barron Henderson <barronh@unc.edu>
+"""
+from netCDF4 import Dataset as NetCDFFile
+
