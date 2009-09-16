d8a261abca17dd9c787a467d29b8e07ea1ea697a
barronh <barronh@f2e443aa-f90a-0410-907f-bad4a36f275b>
2009-09-16 20:05:19 +0000

    Adding leedsmcm as a 'std' mechanism
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/trunk@245 f2e443aa-f90a-0410-907f-bad4a36f275b

--- src/perm/netcdf.py
+++ src/perm/netcdf.py
@@ -12,5 +12,5 @@ __doc__ = """
               selects it and provides it.
 .. moduleauthor:: Barron Henderson <barronh@unc.edu>
 """
-from pupynere import NetCDFFile as NetCDFFile
+from netCDF4 import Dataset as NetCDFFile
 
