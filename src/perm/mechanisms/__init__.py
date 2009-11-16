ca15c54cc4ee1594c03e7461e3d3e17b2fdd69f7
barronh <barronh@f2e443aa-f90a-0410-907f-bad4a36f275b>
2009-11-16 04:25:06 +0000

    Added MOZART chemistry from default inputs
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/trunk@272 f2e443aa-f90a-0410-907f-bad4a36f275b

--- src/perm/mechanisms/__init__.py
+++ src/perm/mechanisms/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['cb05_camx', 'cb05_cmaq', 'geos_chem', 'saprc99_cmaq', 'saprc07_cmaq', 'racm2_cmaq', 'leedsmcm_kpp']
+__all__ = ['cb05_camx', 'cb05_cmaq', 'geos_chem', 'saprc99_cmaq', 'saprc07_cmaq', 'racm2_cmaq', 'leedsmcm_kpp', 'mz4_kpp']
 from ..getmech import get_pure_mech
 
 for mech in __all__:
