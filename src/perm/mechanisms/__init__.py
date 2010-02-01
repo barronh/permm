28f29fa6fb402cfe8de69b876be5549fb0a8623c
barronh <barronh@f2e443aa-f90a-0410-907f-bad4a36f275b>
2010-02-01 17:00:31 +0000

    Adding atoms
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/net_balance/trunk@287 f2e443aa-f90a-0410-907f-bad4a36f275b

--- src/perm/mechanisms/__init__.py
+++ src/perm/mechanisms/__init__.py
@@ -1,5 +1,11 @@
 __all__ = ['cb05_camx', 'cb05_cmaq', 'geos_chem', 'saprc99_cmaq', 'saprc07_cmaq', 'racm2_cmaq', 'leedsmcm_kpp', 'mz4_kpp']
 from ..getmech import get_pure_mech
+from yaml import load
+from os.path import dirname, \
+                    abspath, \
+                    join
+atoms_path = abspath(join(dirname(__file__), 'atoms.yaml'))
 
+atoms = load(file(atoms_path).read())
 for mech in __all__:
     exec('def %s():\n    return get_pure_mech("%s")' % (mech, mech), globals(), locals())
