3bd0af181594c40a78da7855e9ae2fea86c0c65c
Barron Henderson <barronh@gmail.com>
2010-06-07 11:46:45 -0400

    Adding documentation to mechanisms

--- src/permm/mechanisms/__init__.py
+++ src/permm/mechanisms/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['cb05_camx', 'cb05_cmaq', 'geos_chem', 'saprc99_cmaq', 'saprc07_cmaq', 'racm2_cmaq', 'leedsmcm_kpp', 'mz4_kpp']
+__all__ = ['cb05_camx', 'cb05_cmaq', 'geos_chem', 'saprc99_cmaq', 'saprc07_cmaq', 'racm2_cmaq', 'mz4_kpp']
 from ..getmech import get_pure_mech
 from yaml import load
 from os.path import dirname, \
