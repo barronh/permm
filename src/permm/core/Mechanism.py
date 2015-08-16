ca4a314344849510a798f5b774d29fc38aecf1f8
Barron Henderson <barronh@gmail.com>
2015-08-16 07:18:37 -0500

    filter and documentation
    Added ability to filter without specifying reactants and products
    keywords, but using spc keyword with reactant and product species
    objects.
    
    Also added documentation.

--- src/permm/core/Mechanism.py
+++ src/permm/core/Mechanism.py
@@ -134,7 +134,7 @@ class Mechanism(object):
         """
         spc = self.__ensure_species(spc)
         
-        rxns = self.find_rxns(spc,[])
+        rxns = self.find_rxns(reactants = spc)
         return self.__add_spc_to_reactions(rxns, spc)
 
     def add_prd_to_reactions(self, spc):
@@ -144,7 +144,7 @@ class Mechanism(object):
         """
         spc = self.__ensure_species(spc)
         
-        rxns = self.find_rxns([],spc)
+        rxns = self.find_rxns(reactants = [],products = spc)
         return self.__add_spc_to_reactions(rxns, spc)
 
     def add_spc_to_reactions(self, spc):
@@ -173,7 +173,7 @@ class Mechanism(object):
 
         return len(new_rxn_def)
     
-    def get_rxns(self, reactants = [], products =[], logical_and = True, reaction_type = None):
+    def get_rxns(self, reactants = [], products = [], logical_and = True, reaction_type = None):
         """
         Get reaction objects that meet the criteria specified by reactants, products and logical_and.
         
@@ -185,7 +185,7 @@ class Mechanism(object):
         """
         return [self.reaction_dict[rk] for rk in self.find_rxns(reactants = reactants, products = products, logical_and = logical_and, reaction_type = reaction_type)]
 
-    def get_irrs(self, reactants = [], products =[], logical_and = True, reaction_type = None, sortby = None):
+    def get_irrs(self, reactants = [], products = [], logical_and = True, reaction_type = None, sortby = None):
         """
         Get reaction objects that meet the criteria specified by reactants, products and logical_and.
         
@@ -204,7 +204,30 @@ class Mechanism(object):
             result = [r for s, r in result]
             return result
 
-    def find_rxns(self, reactants = [], products =[], logical_and = True, reaction_type = None):
+    def filter_rxns(self, spcs = None, reaction_type = None):
+        """
+        Return a list of filtered reactions.
+        
+        
+        spc - Species with roles specified; single or list of species with roles
+              roles and exclude property affect the outcome.
+        reaction_type - some combination of kjn: thermal (k); photolysis (j); net (n)
+        
+        
+        """
+        result = [(rxn_name, rxn) for rxn_name, rxn in self.reaction_dict.iteritems()]
+        if not reaction_type is None:
+            result = [(rn, rx) for rn, rx in result if rx.reaction_type in reaction_type]
+        
+        if not spcs is None:
+            if not isinstance(spcs, list):
+                spcs = [spcs]
+            spcs = [self.__ensure_species(spc) for spc in spcs]
+        
+            result = [(rn, rx) for rn, rx in result if all([spc in rx for spc in spcs])]
+        return [rn for rn, rx in result]
+    
+    def find_rxns(self, reactants = [], products = [], logical_and = True, reaction_type = None):
         """
         Get reaction names that meet the criteria specified by reactants, products and logical_and.
         
@@ -225,8 +248,7 @@ class Mechanism(object):
         products = [self.__ensure_species(spc) for spc in products]
             
         result = [(rxn_name, rxn) for rxn_name, rxn in self.reaction_dict.iteritems()]
-
-
+        
         if reactants != []:
             reactant_result = [rxn_name for rxn_name, rxn in result if all([rxn.has_rct(rct) for rct in reactants])]
         else:
@@ -236,12 +258,12 @@ class Mechanism(object):
             product_result = [rxn_name for rxn_name, rxn in result if all([rxn.has_prd(prd) for prd in products])]
         else:
             product_result = [rn for rn, rv in result]
-        
+                
             
         if logical_and:
             reaction_list = set(reactant_result).intersection(product_result)
         else:
-            reaction_list = set(reactant_result+product_result)
+            reaction_list = set(reactant_result + product_result)
 
         if reaction_type is not None:
             reaction_result = [rn for rn, rv in result if rv.reaction_type in reaction_type]
@@ -268,6 +290,16 @@ class Mechanism(object):
                         pass
     
     def subst_net_rxn(self, reactants = [], products = [], logical_and = True, reaction_type = None, name = None, netspc = False):
+        """
+        Substitute net reaction for reaction in mechanism.
+        
+        1. Find all matching reactions.
+        2. Create a net reactions from all matching reactions.
+        3. Remove all individual reactions (from e.g., irr_dict, reaction_dict, and variables)
+        4. Add net reaction from step 2 (to e.g., irr_dict, reaction_dict, and variables)
+        
+        Returns: None
+        """
         rxns = self.find_rxns(reactants = reactants, products = products, logical_and = logical_and, reaction_type = reaction_type)
         if len(rxns) > 1:
             eval_str = ' + '.join(rxns)
@@ -286,6 +318,21 @@ class Mechanism(object):
             load_environ(self, self.variables)
 
     def subst_these_rxns(self, rxns, name = None):
+        """
+        
+        Synopsis
+        Substitute net reaction for reactions in mechanism.
+        
+        1. Create a net reactions from all reactions (rxns).
+        2. Remove all individual reactions (from e.g., irr_dict, reaction_dict, and variables)
+        3. Add net reaction from step 1 (to e.g., irr_dict, reaction_dict, and variables)
+        
+        Requires:
+            rxns - iterable of reaction names
+            name - key for new reaction
+        Returns:
+            None
+        """
         if all([isinstance(rxn, Reaction) for rxn in rxns]):
             irrs = rxns
             rxns = []
@@ -322,7 +369,21 @@ class Mechanism(object):
         
         for more information see find_rxns
         """
-        rxns = self.find_rxns(reactants, products, logical_and, reaction_type)
+        rxns = self.find_rxns(reactants = reactants, products = products, logical_and = logical_and, reaction_type = reaction_type)
+        if len(rxns) == 0:
+            return Reaction(stoic = dict())
+        result = self(' + '.join(rxns))
+        
+        return result
+
+    def make_net_filter_rxn(self, spcs = [], reaction_type = None):
+        """
+        Sum each reaction in find_rxns(reactants, procucts, logical_and)
+        and return a net reaction
+        
+        for more information see find_rxns
+        """
+        rxns = self.filter_rxns(spcs = spcs, reaction_type = reaction_type)
         if len(rxns) == 0:
             return Reaction(stoic = dict())
         result = self(' + '.join(rxns))
@@ -336,7 +397,7 @@ class Mechanism(object):
         
         for more information see find_rxns
         """
-        net_rxn = self.make_net_rxn(reactants, products, logical_and, reaction_type)
+        net_rxn = self.make_net_rxn(reactants = reactants, products = products, logical_and = logical_and, reaction_type = reaction_type)
         
         print net_rxn
 
@@ -347,7 +408,7 @@ class Mechanism(object):
         
         for more information see find_rxns
         """
-        rxns = self.find_rxns(reactants, products, logical_and, reaction_type)
+        rxns = self.find_rxns(reactants = reactants, products = products, logical_and = logical_and, reaction_type = reaction_type)
 
         rxns = [(rxn, self.reaction_dict[rxn]) for rxn in rxns]
         try:
@@ -435,7 +496,7 @@ class Mechanism(object):
         """
         if plot_spc is None:
             plot_spc = (_ensure_list(products)+_ensure_list(reactants))[0]
-        reactions = self.find_rxns(reactants, products, logical_and, reaction_type)
+        reactions = self.find_rxns(reactants = reactants, products = products, logical_and = logical_and, reaction_type = reaction_type)
         
         if reactions == []:
             raise ValueError, "Your query didn't match any reactions; check your query and try again (try print_rxns)."
@@ -474,7 +535,7 @@ class Mechanism(object):
         if not hasattr(self,'irr_dict'):
             raise ValueError, "Net reactions are only available when IRR has been loaded"
             
-        rxns = self.find_rxns(reactants, products, logical_and, reaction_type)
+        rxns = self.find_rxns(reactants = reactants, products = products, logical_and = logical_and, reaction_type = reaction_type)
         irrs = [(rxn, self.irr_dict[rxn][slice].sum()) for rxn in rxns]
         
         try:
@@ -493,7 +554,7 @@ class Mechanism(object):
         for rxn, irr in irrs:
             print rxn, (irr * factor).display(nspc = nspc, digits = digits)
         
-    def set_mrg(self,mrg, use_net_rxns = True, use_irr = True, use_ipr = True):
+    def set_mrg(self, mrg, use_net_rxns = True, use_irr = True, use_ipr = True):
         """
         Add process analysis from a 1D merged IRR/IPR file
         """
@@ -512,7 +573,7 @@ class Mechanism(object):
         
         load_environ(self, self.variables)
         
-    def set_irr(self,irr = None, ReactionNames = None, use_net_rxns = True):
+    def set_irr(self, irr = None, ReactionNames = None, use_net_rxns = True):
         """
         Add process analysis from a 2D merged IRR array dim(TIME,RXN)
         """
@@ -587,6 +648,14 @@ class Mechanism(object):
                 self.species_dict[name] = Species(name + ': IGNORE')
 
     def add_rxn(self, rxn_key, rxn_str):
+        """
+        Synopsis:
+            add reaction with name rxn_key and definition rxns_str
+        
+        Requires:
+            rxn_key - string to be used as key for reaction.
+            rxn_str - string that defines the reaction (e.g., N2O + 2 H2O ->[k] HNO3
+        """
         self.reaction_dict[rxn_key] = Reaction(rxn_str)
         if hasattr(self, 'irr_dict'):
             self.irr_dict[rxn_key] = Reaction(rxn_str)
