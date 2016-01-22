c19decb0b38750211a8a9b13a9d653670a6e003d
Barron Henderson <barronh@gmail.com>
2016-01-22 16:03:21 -0500

    setup with download url
    Adding description and download urls for current non-development
    versions.

--- setup.py
+++ setup.py
@@ -40,10 +40,12 @@ setup(name = 'permm',
       author_email = 'barronh@gmail.com',
       maintainer = 'Barron Henderson',
       maintainer_email = 'barronh@gmail.com',
+      description = 'Python Environment for Reaction Mechanisms/Mathematics provides dynamic analysis tools for evaluating chemical networks easily.',
       packages = packages,
       package_dir = {'': 'src'},
       package_data = {'permm': data},
       scripts = ['scripts/permm'],
       requires = ['numpy (>=1.2)', 'yaml', 'netCDF4'],
-      url = 'http://github.com/barronh/permm/'
+      url = 'http://github.com/barronh/permm/',
+      download_url = 'https://github.com/barronh/permm/archive/v1.0.zip'
       )
